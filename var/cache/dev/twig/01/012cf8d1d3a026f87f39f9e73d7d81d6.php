3b8dbb8f1faa6cafb16c0a0724f164232e642fff
conndardspherique <jeremyjardet62@gmail.com>
2025-05-28 14:17:05 +0000

    régler le problème de responsive de merde

--- var/cache/dev/twig/01/012cf8d1d3a026f87f39f9e73d7d81d6.php
+++ var/cache/dev/twig/01/012cf8d1d3a026f87f39f9e73d7d81d6.php
@@ -129,81 +129,122 @@ class __TwigTemplate_29cbfc7b1220400063ec1eabf5abf0bc extends Template
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
 
         // line 11
-        yield "
+        yield "<main>
 <header class=\"header-wrap\">
+    <div class=\"header-logo\">
+        <a class=\"site-logo\" href=\"";
+        // line 14
+        yield $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("app_home");
+        yield "\">
+            <img src=\"";
+        // line 15
+        yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape($this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("images/.png"), "html", null, true);
+        yield "\" alt=\"logo\">
+        </a>
+    </div>
 
-        <div class=\"header-logo\">
-            <a class=\"site-logo\" href=\"index.html\">
-                <img src=\"images/.png\" alt=\"logo\">
-            </a>
-        </div>
-
-        <nav class=\"header-nav-wrap\">
-            <ul id=\"navbar\" class=\"header-main-nav\">
-                <li><a href=\"";
-        // line 22
+    <nav class=\"header-nav-wrap\">
+        <ul id=\"navbar\" class=\"header-main-nav\">
+            <li><a href=\"";
+        // line 21
         yield $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("app_home");
         yield "\">Accueil</a></li>
-               
-               
+        </ul>
+        
+    </nav>
 
+    <a class=\"header-menu-toggle\" href=\"#\"><span>Menu</span></a>
+</header>
 
-
-            </ul>
-
-            <ul class=\"header-social\">
-                <li>
-                    <a class=\"social-icon\" href=\"https://www.facebook.com/LaCrypteTattooStudio\" target=\"_blank\"><span class=\"iconify\" data-icon=\"la:facebook-f\"></span></a>
-                </li>                
-            </ul>
-        </nav>
-
-        <a class=\"header-menu-toggle\" href=\"#\"><span>Menu</span></a>
-
-    </header>
 <link rel=\"preconnect\" href=\"https://fonts.googleapis.com\">
 <link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin>
-<link href=\"https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Rozha+One&display=swap\" rel=\"stylesheet\">
+<link href=\"https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap\" rel=\"stylesheet\">
+
 <div class=\"container\">
-    <h1>Prendre rendez-vous</h1>
-    ";
+    <h1 class=\"rdv-title\">Prendre rendez-vous</h1>
+    <div class=\"form-container\">
+        ";
+        // line 36
+        yield         $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->renderBlock((isset($context["form"]) || array_key_exists("form", $context) ? $context["form"] : (function () { throw new RuntimeError('Variable "form" does not exist.', 36, $this->source); })()), 'form_start');
+        yield "
+            ";
+        // line 37
+        yield $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->searchAndRenderBlock(CoreExtension::getAttribute($this->env, $this->source, (isset($context["form"]) || array_key_exists("form", $context) ? $context["form"] : (function () { throw new RuntimeError('Variable "form" does not exist.', 37, $this->source); })()), "nom", [], "any", false, false, false, 37), 'row');
+        yield "
+            ";
+        // line 38
+        yield $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->searchAndRenderBlock(CoreExtension::getAttribute($this->env, $this->source, (isset($context["form"]) || array_key_exists("form", $context) ? $context["form"] : (function () { throw new RuntimeError('Variable "form" does not exist.', 38, $this->source); })()), "email", [], "any", false, false, false, 38), 'row');
+        yield "
+            ";
+        // line 39
+        yield $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->searchAndRenderBlock(CoreExtension::getAttribute($this->env, $this->source, (isset($context["form"]) || array_key_exists("form", $context) ? $context["form"] : (function () { throw new RuntimeError('Variable "form" does not exist.', 39, $this->source); })()), "telephone", [], "any", false, false, false, 39), 'row');
+        yield "
+            ";
+        // line 40
+        yield $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->searchAndRenderBlock(CoreExtension::getAttribute($this->env, $this->source, (isset($context["form"]) || array_key_exists("form", $context) ? $context["form"] : (function () { throw new RuntimeError('Variable "form" does not exist.', 40, $this->source); })()), "date", [], "any", false, false, false, 40), 'row');
+        yield "
+            ";
+        // line 41
+        yield $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->searchAndRenderBlock(CoreExtension::getAttribute($this->env, $this->source, (isset($context["form"]) || array_key_exists("form", $context) ? $context["form"] : (function () { throw new RuntimeError('Variable "form" does not exist.', 41, $this->source); })()), "objet", [], "any", false, false, false, 41), 'row');
+        yield "
+            ";
+        // line 42
+        yield $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->searchAndRenderBlock(CoreExtension::getAttribute($this->env, $this->source, (isset($context["form"]) || array_key_exists("form", $context) ? $context["form"] : (function () { throw new RuntimeError('Variable "form" does not exist.', 42, $this->source); })()), "message", [], "any", false, false, false, 42), 'row');
+        yield "
+            ";
+        // line 43
+        yield $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->searchAndRenderBlock(CoreExtension::getAttribute($this->env, $this->source, (isset($context["form"]) || array_key_exists("form", $context) ? $context["form"] : (function () { throw new RuntimeError('Variable "form" does not exist.', 43, $this->source); })()), "fichier", [], "any", false, false, false, 43), 'row');
+        yield "
+            <button type=\"submit\" class=\"btn-submit\">Envoyer</button>
+        ";
         // line 45
-        yield         $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->renderBlock((isset($context["form"]) || array_key_exists("form", $context) ? $context["form"] : (function () { throw new RuntimeError('Variable "form" does not exist.', 45, $this->source); })()), 'form_start');
+        yield         $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->renderBlock((isset($context["form"]) || array_key_exists("form", $context) ? $context["form"] : (function () { throw new RuntimeError('Variable "form" does not exist.', 45, $this->source); })()), 'form_end');
         yield "
-        ";
-        // line 46
-        yield $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->searchAndRenderBlock(CoreExtension::getAttribute($this->env, $this->source, (isset($context["form"]) || array_key_exists("form", $context) ? $context["form"] : (function () { throw new RuntimeError('Variable "form" does not exist.', 46, $this->source); })()), "nom", [], "any", false, false, false, 46), 'row');
-        yield "
-        ";
-        // line 47
-        yield $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->searchAndRenderBlock(CoreExtension::getAttribute($this->env, $this->source, (isset($context["form"]) || array_key_exists("form", $context) ? $context["form"] : (function () { throw new RuntimeError('Variable "form" does not exist.', 47, $this->source); })()), "email", [], "any", false, false, false, 47), 'row');
-        yield "
-        ";
-        // line 48
-        yield $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->searchAndRenderBlock(CoreExtension::getAttribute($this->env, $this->source, (isset($context["form"]) || array_key_exists("form", $context) ? $context["form"] : (function () { throw new RuntimeError('Variable "form" does not exist.', 48, $this->source); })()), "telephone", [], "any", false, false, false, 48), 'row');
-        yield "
-        ";
-        // line 49
-        yield $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->searchAndRenderBlock(CoreExtension::getAttribute($this->env, $this->source, (isset($context["form"]) || array_key_exists("form", $context) ? $context["form"] : (function () { throw new RuntimeError('Variable "form" does not exist.', 49, $this->source); })()), "date", [], "any", false, false, false, 49), 'row');
-        yield "
-        ";
-        // line 50
-        yield $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->searchAndRenderBlock(CoreExtension::getAttribute($this->env, $this->source, (isset($context["form"]) || array_key_exists("form", $context) ? $context["form"] : (function () { throw new RuntimeError('Variable "form" does not exist.', 50, $this->source); })()), "objet", [], "any", false, false, false, 50), 'row');
-        yield "
-        ";
-        // line 51
-        yield $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->searchAndRenderBlock(CoreExtension::getAttribute($this->env, $this->source, (isset($context["form"]) || array_key_exists("form", $context) ? $context["form"] : (function () { throw new RuntimeError('Variable "form" does not exist.', 51, $this->source); })()), "message", [], "any", false, false, false, 51), 'row');
-        yield "
-        ";
-        // line 52
-        yield $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->searchAndRenderBlock(CoreExtension::getAttribute($this->env, $this->source, (isset($context["form"]) || array_key_exists("form", $context) ? $context["form"] : (function () { throw new RuntimeError('Variable "form" does not exist.', 52, $this->source); })()), "fichier", [], "any", false, false, false, 52), 'row');
-        yield "
-        <button type=\"submit\">ENVOYER</button>
-    ";
-        // line 54
-        yield         $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->renderBlock((isset($context["form"]) || array_key_exists("form", $context) ? $context["form"] : (function () { throw new RuntimeError('Variable "form" does not exist.', 54, $this->source); })()), 'form_end');
-        yield "
+    </div>
 </div>
+
+<footer style=\"background-color:#222; color:#eee; padding:20px 10px; text-align:center; font-family:Arial, sans-serif; font-size:14px;\">
+  <div class=\"footer-links\" style=\"margin-bottom:10px;\">
+    <a href=\"/mentions-legales\" class=\"footer-link\">Mentions légales</a> |
+    <a href=\"/conditions-generales\" class=\"footer-link\">Conditions générales</a> |
+    <a href=\"/politique-confidentialite\" class=\"footer-link\">Politique de confidentialité</a> |
+    <a href=\"/contact\" class=\"footer-link\">Contact</a>
+  </div>
+  <div style=\"color:#666;\">
+    &copy; 2025 Profane Tattoo. Tous droits réservés.
+  </div>
+
+  <style>
+    .footer-link {
+      color: #aaa;
+      margin: 0 8px;
+      text-decoration: none;
+      transition: color 0.3s ease;
+    }
+    .footer-link:hover, .footer-link:focus {
+      color: #1e90ff; /* bleu clair au hover */
+      text-decoration: underline;
+      outline: none;
+    }
+
+    /* Responsive */
+    @media (max-width: 480px) {
+      footer {
+        font-size: 13px;
+        padding: 15px 5px;
+      }
+      .footer-links {
+        display: flex;
+        flex-direction: column;
+        gap: 8px;
+      }
+      .footer-link {
+        margin: 0;
+      }
+    }
+  </style>
+</footer>
+</main>
 ";
         
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
@@ -235,7 +276,7 @@ class __TwigTemplate_29cbfc7b1220400063ec1eabf5abf0bc extends Template
      */
     public function getDebugInfo(): array
     {
-        return array (  204 => 54,  199 => 52,  195 => 51,  191 => 50,  187 => 49,  183 => 48,  179 => 47,  175 => 46,  171 => 45,  145 => 22,  132 => 11,  119 => 10,  106 => 7,  101 => 6,  88 => 5,  65 => 3,  42 => 1,);
+        return array (  201 => 45,  196 => 43,  192 => 42,  188 => 41,  184 => 40,  180 => 39,  176 => 38,  172 => 37,  168 => 36,  150 => 21,  141 => 15,  137 => 14,  132 => 11,  119 => 10,  106 => 7,  101 => 6,  88 => 5,  65 => 3,  42 => 1,);
     }
 
     public function getSourceContext(): Source
@@ -250,54 +291,87 @@ class __TwigTemplate_29cbfc7b1220400063ec1eabf5abf0bc extends Template
 {% endblock %}
 
 {% block body %}
-
+<main>
 <header class=\"header-wrap\">
+    <div class=\"header-logo\">
+        <a class=\"site-logo\" href=\"{{ path('app_home') }}\">
+            <img src=\"{{ asset('images/.png') }}\" alt=\"logo\">
+        </a>
+    </div>
 
-        <div class=\"header-logo\">
-            <a class=\"site-logo\" href=\"index.html\">
-                <img src=\"images/.png\" alt=\"logo\">
-            </a>
-        </div>
+    <nav class=\"header-nav-wrap\">
+        <ul id=\"navbar\" class=\"header-main-nav\">
+            <li><a href=\"{{ path('app_home') }}\">Accueil</a></li>
+        </ul>
+        
+    </nav>
 
-        <nav class=\"header-nav-wrap\">
-            <ul id=\"navbar\" class=\"header-main-nav\">
-                <li><a href=\"{{ path('app_home') }}\">Accueil</a></li>
-               
-               
+    <a class=\"header-menu-toggle\" href=\"#\"><span>Menu</span></a>
+</header>
 
-
-
-            </ul>
-
-            <ul class=\"header-social\">
-                <li>
-                    <a class=\"social-icon\" href=\"https://www.facebook.com/LaCrypteTattooStudio\" target=\"_blank\"><span class=\"iconify\" data-icon=\"la:facebook-f\"></span></a>
-                </li>                
-            </ul>
-        </nav>
-
-        <a class=\"header-menu-toggle\" href=\"#\"><span>Menu</span></a>
-
-    </header>
 <link rel=\"preconnect\" href=\"https://fonts.googleapis.com\">
 <link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin>
-<link href=\"https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Rozha+One&display=swap\" rel=\"stylesheet\">
+<link href=\"https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap\" rel=\"stylesheet\">
+
 <div class=\"container\">
-    <h1>Prendre rendez-vous</h1>
-    {{ form_start(form) }}
-        {{ form_row(form.nom) }}
-        {{ form_row(form.email) }}
-        {{ form_row(form.telephone) }}
-        {{ form_row(form.date) }}
-        {{ form_row(form.objet) }}
-        {{ form_row(form.message) }}
-        {{ form_row(form.fichier) }}
-        <button type=\"submit\">ENVOYER</button>
-    {{ form_end(form) }}
+    <h1 class=\"rdv-title\">Prendre rendez-vous</h1>
+    <div class=\"form-container\">
+        {{ form_start(form) }}
+            {{ form_row(form.nom) }}
+            {{ form_row(form.email) }}
+            {{ form_row(form.telephone) }}
+            {{ form_row(form.date) }}
+            {{ form_row(form.objet) }}
+            {{ form_row(form.message) }}
+            {{ form_row(form.fichier) }}
+            <button type=\"submit\" class=\"btn-submit\">Envoyer</button>
+        {{ form_end(form) }}
+    </div>
 </div>
+
+<footer style=\"background-color:#222; color:#eee; padding:20px 10px; text-align:center; font-family:Arial, sans-serif; font-size:14px;\">
+  <div class=\"footer-links\" style=\"margin-bottom:10px;\">
+    <a href=\"/mentions-legales\" class=\"footer-link\">Mentions légales</a> |
+    <a href=\"/conditions-generales\" class=\"footer-link\">Conditions générales</a> |
+    <a href=\"/politique-confidentialite\" class=\"footer-link\">Politique de confidentialité</a> |
+    <a href=\"/contact\" class=\"footer-link\">Contact</a>
+  </div>
+  <div style=\"color:#666;\">
+    &copy; 2025 Profane Tattoo. Tous droits réservés.
+  </div>
+
+  <style>
+    .footer-link {
+      color: #aaa;
+      margin: 0 8px;
+      text-decoration: none;
+      transition: color 0.3s ease;
+    }
+    .footer-link:hover, .footer-link:focus {
+      color: #1e90ff; /* bleu clair au hover */
+      text-decoration: underline;
+      outline: none;
+    }
+
+    /* Responsive */
+    @media (max-width: 480px) {
+      footer {
+        font-size: 13px;
+        padding: 15px 5px;
+      }
+      .footer-links {
+        display: flex;
+        flex-direction: column;
+        gap: 8px;
+      }
+      .footer-link {
+        margin: 0;
+      }
+    }
+  </style>
+</footer>
+</main>
 {% endblock %}
-
-
 ", "rendezvous.html.twig", "/home/jeremy/profane/Profane/templates/rendezvous.html.twig");
     }
 }
